fdf70ebe3c5175fec7018dd117b4eeb693c17019
Saya <chu121su12@gmail.com>
2022-01-18 21:31:48 +0800

    wip

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::group(base_path('routes/routes.php'));
+            Route::group([], base_path('routes/routes.php'));
         });
     }
 
